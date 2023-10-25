f75cae8d3d75db0b595b70632453de00df05d41b
konductor000 <skorodumov000@mail.ru>
2023-10-25 16:55:02 +0300

    continue training large

--- cf.py
+++ cf.py
@@ -1,28 +1,57 @@
-def solve(n):
-    left = 1
-    right = n + 2
-    ans = 10 ** 20
+from time import time
+import psutil
 
-    while left <= right:
-        mid = (left + right) // 2
-        if mid * (mid-1) // 2 > n:
-            right = mid - 1
+
+class Employee():
+    def __init__(self, personal_number, sup, lang):
+        self.personal_number = personal_number
+        self.sup = sup
+        self.lang = lang
+
+        self.bariers = {}
+
+        if sup is None:
+            self.bariers = {"A": 0, "B": 0}
+        elif lang == "A":
+            self.bariers[lang] = 0
+            self.bariers["B"] = sup.bariers["B"] + 1
         else:
-            left = mid + 1
-            ans = min(ans, mid+n-mid*(mid-1)//2)
-    
-    print(ans)
+            self.bariers[lang] = 0
+            self.bariers["A"] = sup.bariers["A"] + 1
 
-
-def read(inputs):
+def solve():
     n = int(input())
-    inputs.append(n)
+    langs = [i for i in input().split()]
+    people = [int(i) for i in input().split()]
+    process = psutil.Process()
+    print(f"Initial memory usage: {process.memory_info().rss / 1024 / 1024} MB")
 
+    start = time()
+    n = 10**6
+    langs = ["A"]*n
+    people = [i for i in range(n+1)] + [i for i in range(n, -1, -1)]
+    employees = {}
 
-inputs = []
-t = int(input())
-for _ in range(t):
-    read(inputs)
+    sup = Employee(0, None, "AB")
+    for i in range(1, len(people)):
+        personal_number = people[i]
+        lang = langs[personal_number - 1]
+        if personal_number == 0:
+            continue
+        elif personal_number == sup.personal_number:
+            sup = sup.sup
+        else:
+            employee  = Employee(personal_number, sup, lang)
+            employees[personal_number] = employee
+            sup = employee
 
-for i in range(t):
-    solve(inputs[i])
+    bariers = []
+    for i in range(1, n + 1):
+        employee = employees[i]
+        bariers.append(str(employee.sup.bariers[employee.lang]))
+
+    #print(" ".join(bariers))
+    print(time()-start)
+    print(f"Final memory usage: {process.memory_info().rss / 1024 / 1024} MB")
+
+solve()
